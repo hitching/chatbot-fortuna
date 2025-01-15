7d1f2b84f681b535b2292ef71464a98e271b30e0
Bob Hitching <bob@hitching.net>
2025-01-15 03:52:21 +0000

    first

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,55 +2,40 @@ import streamlit as st
 from openai import OpenAI
 
 # Show title and description.
-st.title("💬 Chatbot")
+st.title("💬 Fortuna Admissions AI Chat")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "Chat privately with our AI about your background and MBA goals, and how Fortuna can help."
 )
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
+if "ai_response" not in st.session_state: 
+    st.session_state.ai_response = None
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+client = OpenAI(api_key=st.secrets["openai_key"])
+def submit_enquiry():
+    enquiry = f"I need help applying to MBA schools. My goals are: {goals}. My background: {background}."
+    prompt = f"You are the receptionist at Fortuna Admissions, the dream team of fictional movie characters and former Admissions Directors from the world\'s top business schools, providing advisory services to help MBA applicants strengthen their profile, position their application and target the best schools. Respond to the following enquiry from an MBA applicant with 3 answers, each from a fictional movie character or Heidi Hillis a Fortuna Consultant, each giving an in-character response to the enquiry. For each response, introduce yourself, compliment the applicant on a particular aspect of their background or experience, and explain how your own experience is relevant to helping the applicant, then explain the help available, including essay editing and mock interviews, asking an insightful and deeply probing hypothetical question about the background of the applicant as an example of what might be asked for an essay or mock interview. Finally mention a date and time when the character is next available for a consultation, and ask for the applicant to provide contact details as a next step. Here's the enquiry: {enquiry}."
 
-    # Create a session state variable to store the chat messages. This ensures that the
-    # messages persist across reruns.
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+    st.session_state.ai_response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": prompt}
+        ],
+        stream=True,
+    )
 
-    # Display the existing chat messages via `st.chat_message`.
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+background = st.text_area('Tell us about your background. Paste in your resume, or parts of your LinkedIn profile, or write a brief summary. Your name and contact details are not required.')
 
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
+if background:
+    goals = st.text_area('What are your MBA goals? Which schools and programs are you interested in applying for?')
 
-        # Store and display the current prompt.
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
-
-        # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
+    if goals:
+        country = st.selectbox(
+            'And finally, what\'s your country of residence?',
+            ['', 'Australia', 'United States']
         )
 
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
-        with st.chat_message("assistant"):
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+        submit_button = st.button('Submit', type="primary", on_click=submit_enquiry, disabled=not country)
+
+if st.session_state.ai_response:
+    with st.chat_message("assistant"):
+        st.write_stream(st.session_state.ai_response)
